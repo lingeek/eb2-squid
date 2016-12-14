507b2040f92772eefadc239343a87d49787b80b9
lingeek <raul@deva-alpine-adventure.org>
2016-12-14 06:39:31 -0500

    fixing class

---
+++ manifests/service.pp
@@ -0,0 +1,31 @@
+#Service class
+class eb2_squid::service (
+
+#THis value is telling us if the service should be enabled on the system
+$service_ensure = hiera('squid3::service::active'),
+#END
+
+#THis value is telling us if the service should be running or NOT
+$service_status = hiera('squid3::service::status'),
+#END
+
+#This is the service name which is different from OS to OS
+$service_name   = hiera('squid3::service::name'),
+#END
+
+){
+
+
+
+service { $service_name:
+    ensure    => $service_ensure,
+    enable    => $service_status,
+    name      => $service_name,
+    hasstatus => true,
+    require   => Class['eb2_squid::install'],
+  }
+
+
+
+
+}
